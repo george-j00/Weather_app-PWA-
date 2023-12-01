d86836a7d170440dd0db9faa105ef7f587f64745
GeorgeJose <subingeorge027@gmail.com>
2023-12-01 21:40:00 +0530

    Update: updated vite.config for pwq

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,59 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'masked-icon.svg',
+        '/android-chrome-192x192.png',
+        '/android-chrome-512x512.png',
+        '/apple-touch-icon.png',
+        '/maskable_icon.png',
+      ],
+      manifest: {
+        name: "React-vite-app",
+        short_name: "react-vite-app",
+        description: "Simple Weather PWA app ",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#171717",
+        background_color: "#f0e7db",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+      },
+    }),
+  ],
+});
