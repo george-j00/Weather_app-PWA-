a1229a8f9a335b6c38c0125b2b5508b845fdbe66
GeorgeJose <subingeorge027@gmail.com>
2023-12-01 22:05:38 +0530

    Done: Adds cloudinary for video store

--- src/components/BgVideo.tsx
+++ src/components/BgVideo.tsx
@@ -4,17 +4,16 @@ import { RootState } from "../ReduxStore/store";
 const BgVideo = () => {
   const currentState = useSelector((state: RootState) => state.climate.current);
   
-  const base_url = "/src/assets/";
   const videoUrl =
     currentState?.condition.text === "Mist"
-      ? base_url + "mist.mp4"
+      ? "https://res.cloudinary.com/dyawq6e7r/video/upload/v1701448143/mist_m7bxfh.mp4"
       : currentState?.condition.text === "Partly cloudy "
-      ? base_url + "cloudy.mp4"
+      ? "https://res.cloudinary.com/dyawq6e7r/video/upload/v1701447984/slu6zxtm2utbwfsyqi1i.mp4"
       : currentState?.condition.text === "Patchy rain possible"
-      ? base_url + "raining.mp4"
+      ? "https://res.cloudinary.com/dyawq6e7r/video/upload/v1701448236/raining_yzxcop.mp4"
       : currentState?.condition.text === "Sunny"
-      ? base_url + "sunny.mp4"
-      : base_url + "cloudy.mp4";
+      ? "https://res.cloudinary.com/dyawq6e7r/video/upload/v1701448356/sunny_yhecfv.mp4"
+      : "https://res.cloudinary.com/dyawq6e7r/video/upload/v1701447984/slu6zxtm2utbwfsyqi1i.mp4";
 
   return (
     <>
